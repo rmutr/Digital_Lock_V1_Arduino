74985369e6c6b29ea75e4a59a4e5450033adf46b
rmutr <sanya.sam@rmutr.ac.th>
2020-04-26 20:56:18 +0700

    First sync.

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+//----------------------------------------------------------------------------- 
+// Project          : RmutR Digital Lock V1 
+// VSCode Extension : PlatformIO IDE 1.10.0 
+// Source           : https://github.com/rmutr/Digital_Lock_V1.git 
+// Board            : Node32s (Gravitech Node32Lite LamLoei) 
+// Additional URLs  : https://dl.espressif.com/dl/package_esp32_index.json 
+// LED_BUILTIN      : Pin 2 
+
+
+//----------------------------------------------------------------------------- 
+#include <Arduino.h> 
+
+
+//----------------------------------------------------------------------------- 
+void setup() { 
+  Serial.begin(115200); 
+  Serial.println("Digital Lock V1"); 
+
+} 
+
+void loop() { 
+
+} 
+
