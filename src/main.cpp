d1e85e494644c35336a2d9ef04f8c2eca337df83
rmutr <sanya.sam@rmutr.ac.th>
2020-04-28 14:24:46 +0700

    Add bluetooth communication.

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,11 @@
 
 #include <LiquidCrystal_PCF8574.h>
 
+#include <BLEDevice.h> 
+#include <BLEServer.h> 
+#include <BLEUtils.h> 
+#include <BLE2902.h> 
+
 #define PIN_RX_0             3 
 #define PIN_TX_0             1 
 
@@ -41,6 +46,8 @@ int state_ix_mon = 0;
 
 volatile int req_machine_start = 0; 
 
+int bluetooth_linked = 0; 
+
 //----------------------------------------------------------------------------- 
 #define SCREEN_WIDTH       128 
 #define SCREEN_HEIGHT       64 
@@ -64,9 +71,50 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 LiquidCrystal_PCF8574 clcd(CLCD_ADD_27); 
 
 //----------------------------------------------------------------------------- 
+BLEServer *pServer; 
+BLECharacteristic *pTxCharacteristic; 
+bool deviceConnected = false; 
+bool oldDeviceConnected = false; 
+uint8_t txvalue = 0; 
+
+// See the following for generating UUIDs: 
+// https://www.uuidgenerator.net/ 
+
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" 
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" 
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" 
+
+//----------------------------------------------------------------------------- 
 void Interrupt_Service_Btn_Start(); 
 
 void Interrupt_Service_Btn_Start() { req_machine_start = 1; } 
+
+class MyServerCallbacks: public BLEServerCallbacks { 
+  void onConnect(BLEServer* pServer) { 
+    deviceConnected = true; 
+    //BLEDevice::startAdvertising(); 
+  }; 
+
+  void onDisconnect(BLEServer* pServer) { 
+    deviceConnected = false; 
+  } 
+}; 
+
+class MyCallbacks: public BLECharacteristicCallbacks { 
+  void onWrite(BLECharacteristic *pCharacteristic) { 
+    std::string rxvalue = pCharacteristic->getValue(); 
+
+    if (rxvalue.length() > 0) { 
+
+      Serial.print("Received Value: "); 
+      for (int i = 0; i < rxvalue.length(); i++) { 
+        Serial.print(rxvalue[i]); 
+      } 
+      Serial.println(); 
+
+    } 
+  } 
+}; 
 
 
 //----------------------------------------------------------------------------- 
@@ -101,19 +149,70 @@ void setup() {
   clcd.print(buff_str); 
 
 //----------------------------------------------------------------------------- 
+  BLEDevice::init(buff_str); 
+
+  pServer = BLEDevice::createServer(); 
+  pServer->setCallbacks(new MyServerCallbacks()); 
+
+  BLEService *pService = pServer->createService(SERVICE_UUID); 
+
+  pTxCharacteristic = pService->createCharacteristic( 
+                        CHARACTERISTIC_UUID_TX, 
+                        BLECharacteristic::PROPERTY_NOTIFY 
+                      ); 
+
+  pTxCharacteristic->addDescriptor(new BLE2902()); 
+
+  BLECharacteristic *pRxCharacteristic = pService->createCharacteristic( 
+                       CHARACTERISTIC_UUID_RX, 
+                       BLECharacteristic::PROPERTY_WRITE 
+                     ); 
+
+  pRxCharacteristic->setCallbacks(new MyCallbacks()); 
+
+  pService->start(); 
+
+  pServer->getAdvertising()->start(); 
+  Serial.println("Waiting a client connection to notify..."); 
+
+//----------------------------------------------------------------------------- 
   t_old = 0; 
   tmr_cnt = 0; 
   error = 0; 
   state_ix = 0; 
   state_ix_mon = 0; 
   req_machine_start = 0; 
+  bluetooth_linked = 0; 
 
 } 
 
 void loop() { 
+  //connected 
+  bluetooth_linked = 0; 
+  if (deviceConnected) { 
+    bluetooth_linked = 1; 
+    char bstr[50] = {0}; sprintf(bstr, "%02d", txvalue); 
+    pTxCharacteristic->setValue(bstr); 
+    pTxCharacteristic->notify(); 
+    txvalue++; if (txvalue >= 100) { txvalue = 0; } 
+    delay(1000); 
+  } 
+
+  // disconnecting 
+  if (!deviceConnected && oldDeviceConnected) { 
+    delay(500); // give the bluetooth stack the chance to get things ready 
+    pServer->startAdvertising(); // restart advertising 
+    Serial.println("start advertising"); 
+    oldDeviceConnected = deviceConnected; 
+  } 
+
+  // connecting 
+  if (deviceConnected && !oldDeviceConnected) { 
+    oldDeviceConnected = deviceConnected; 
+  } 
 
 //----------------------------------------------------------------------------- 
-  sprintf(buff_str, " S-%02d L-%d SW-%d | ", state_ix_mon, 0, req_machine_start); 
+  sprintf(buff_str, " S-%02d L-%d SW-%d | ", state_ix_mon, bluetooth_linked, req_machine_start); 
   Serial.println(buff_str); 
 
 //----------------------------------------------------------------------------- 
@@ -121,4 +220,5 @@ void loop() {
   tmr_cnt++; if (tmr_cnt >= 10) { tmr_cnt = 0; } 
 
 } 
+
 
